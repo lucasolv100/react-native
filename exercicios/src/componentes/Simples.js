a4a316a556da025b8a947dd22f97f5e4ac873e2a
Lucas <lucas@smcaetano.com>
2020-06-23 10:57:31 -0300

    adicionado primeiro componente funcional

---
+++ exercicios/src/componentes/Simples.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+// export default (props) => {
+//     return <Text>Simples!! {props.propPai}</Text>
+// }
+export default props => <Text>Simples reduzido!! {props.propPai}</Text>
